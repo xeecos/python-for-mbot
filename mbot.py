5873461174750066e03b5ede34d909f6223716f8
xeecos <flashindream@gmail.com>
2015-11-21 23:39:03 +0800

    for class

--- mbot.py
+++ mbot.py
@@ -1,136 +1,65 @@
 import serial
-import sys,threading,time,signal
+import sys,time,signal
+from multiprocessing import Process
 from time import ctime,sleep
 import struct
 import glob
+import copy
 
-buffer = []
-packageList = []
-selectors = []
-bufferIndex = 0
-timeIndex = 0
-isParseStart = False
-isParseStartIndex = 0
-is_loop = True
 led = 1
+timeIndex = 0
 
-def serialPorts():
-	if sys.platform.startswith('win'):
-		ports = ['COM%s' % (i + 1) for i in range(256)]
-	elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-		ports = glob.glob('/dev/tty[A-Za-z]*')
-	elif sys.platform.startswith('darwin'):
-		ports = glob.glob('/dev/tty.*')
-	else:
-		raise EnvironmentError('Unsupported platform')
-	result = []
-	for port in ports:
-		s = serial.Serial()
-		s.port = port
-		s.close()
-		result.append(port)
-	return result
+class mSerial():
+	ser = None
+	def start(self,port,callback):
+		self.ser = serial.Serial(port,115200)
+		self.parseBuffer = callback
+		th = Process(target=self.onRead)
+		th.start()
 
-def parseBuffer(byte):
-	global buffer,isParseStart,isParseStartIndex
-	position = 0
-	value = 0	
-	buffer+=[byte]
-	bufferLength = len(buffer)
-	if bufferLength >= 2:
-		if (buffer[bufferLength-1]==0x55 and buffer[bufferLength-2]==0xff):
-			isParseStart = True
-			isParseStartIndex = bufferLength-2	
-		if (buffer[bufferLength-1]==0xa and buffer[bufferLength-2]==0xd and isParseStart==True):			
-			isParseStart = False
-			position = isParseStartIndex+2
-			extId = buffer[position]
-			position+=1
-			type = buffer[position]
-			position+=1
-			# 1 byte 2 float 3 short 4 len+string 5 double
-			if type == 1:
-				value = buffer[position]
-			if type == 2:
-				value = readFloat(position)
-				if(value<-255 or value>1023):
-					value = 0
-			if type == 3:
-				value = readShort(position)
-			if type == 4:
-				value = readString(position)
-			if type == 5:
-				value = readDouble(position)
-			if(type<=5):
-				responseValue(extId,value)
-			buffer = []
-def readFloat(position):
-	v = [buffer[position], buffer[position+1],buffer[position+2],buffer[position+3]]
-	return struct.unpack('<f', struct.pack('4B', *v))[0]
-def readShort(position):
-	v = [buffer[position], buffer[position+1]]
-	return struct.unpack('<h', struct.pack('2B', *v))[0]
-def readString(position):
-	l = buffer[position]
-	position+=1
-	s = ""
-	for i in Range(l):
-		s += buffer[position+i].charAt(0)
-	return s
-def readDouble(position):
-	v = [buffer[position], buffer[position+1],buffer[position+2],buffer[position+3]]
-	return struct.unpack('<f', struct.pack('4B', *v))[0]
+	def serialPorts(self):
+		if sys.platform.startswith('win'):
+			ports = ['COM%s' % (i + 1) for i in range(256)]
+		elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+			ports = glob.glob('/dev/tty[A-Za-z]*')
+		elif sys.platform.startswith('darwin'):
+			ports = glob.glob('/dev/tty.*')
+		else:
+			raise EnvironmentError('Unsupported platform')
+		result = []
+		for port in ports:
+			s = serial.Serial()
+			s.port = port
+			s.close()
+			result.append(port)
+		return result
 
-def responseValue(extID,value):
-	try:	
-		selectors[extID](value) 
-	except Exception,ex:
-			print str(ex)
+	def onRead(self):
+		while True:
+			try:	
+				if self.ser.isOpen()==True:
+					n = self.ser.inWaiting()
+					if n>0: 
+						self.parseBuffer(ord(self.ser.read()))
+					else:
+						sleep(0.01)
+				else:	
+					sleep(0.5)
+			except Exception,ex:
+				print str(ex)
 
-def readLoop():
-	global is_loop
-	while is_loop:
-		try:	
-			if ser.isOpen():
-				n = ser.inWaiting()
-				if n>0: 
-					parseBuffer(ord(ser.read()))
-				else:
-					sleep(0.01)
-				requestData()
-			else:	
-				sleep(0.5)
-		except Exception,ex:
-			print str(ex)
-def requestData():
-	global timeIndex,led,bot
-	timeIndex += 1
-	if timeIndex > 50:
-		timeIndex = 0
-		try:	
-			if ser.isOpen():	
-				led = 3 - led
-				addPackage(bot.packageRGBLedOnBoard(0x0,0x0,0x0,0x0))
-				addPackage(bot.packageRGBLedOnBoard(led,0x0,0x10,0x0))
-				# addPackage(bot.packageBuzzer(led*200))
-				addPackage(bot.requestLightOnBoard(8,onLight))
-		except Exception,ex:
-			print str(ex)
-
-def onLight(value):
-	print value
-
-def addPackage(package):
-	packageList.append(package)
-	sendFirstPackage()
-
-def sendFirstPackage():
-	if len(packageList) > 0:
-		ser.write(packageList[0])
-		packageList.pop(0)
+	def writePackage(self,package):
+		self.ser.write(package)
 		sleep(0.01)
 
 class mBot():
+	buffer = []
+	bufferIndex = 0
+	isParseStart = False
+	isParseStartIndex = 0
+	def __init__(self):
+		self.selectors = {}
+
 	def packageRGBLed(self,port,index,red,green,blue):
 		return bytearray([0xff,0x55,0x8,0x0,0x2,0x8,port,index,red,green,blue])
 
@@ -156,61 +85,123 @@ class mBot():
 		return bytearray([0xff,0x55,len(message)+3,0x0,0x2,0xd,message])
 		
 	def requestLightOnBoard(self,extID,callback):
-		setSelector(extID,callback)
-		return self.requestLight(extID,8)
+		return self.requestLight(extID,8,callback)
 	
-	def requestLight(self,extID,port):
+	def requestLight(self,extID,port,callback):
+		self.setCallback(extID,callback)
 		return bytearray([0xff,0x55,0x4,extID,0x1,0x3,port])
 
-	def requestButtonOnBoard(self,extID):
+	def requestButtonOnBoard(self,extID,callback):
+		self.setCallback(extID,callback)
 		return bytearray([0xff,0x55,0x4,extID,0x1,0x1f,0x7])
 		
-	def requestIROnBoard(self,extID):
+	def requestIROnBoard(self,extID,callback):
+		self.setCallback(extID,callback)
 		return bytearray([0xff,0x55,0x3,extID,0x1,0xd])
 		
-	def requestUltrasonicSensor(self,extID,port):
+	def requestUltrasonicSensor(self,extID,port,callback):
+		self.setCallback(extID,callback)
 		return bytearray([0xff,0x55,0x4,extID,0x1,0x1,port])
 		
-	def requestLineFollower(self,extID,port):
+	def requestLineFollower(self,extID,port,callback):
+		self.setCallback(extID,callback)
 		return bytearray([0xff,0x55,0x4,extID,0x1,0x11,port])
+	
+	def parseBuffer(self, byte):
+		position = 0
+		value = 0	
+		self.buffer+=[byte]
+		bufferLength = len(self.buffer)
+		if bufferLength >= 2:
+			if (self.buffer[bufferLength-1]==0x55 and self.buffer[bufferLength-2]==0xff):
+				self.isParseStart = True
+				self.isParseStartIndex = bufferLength-2	
+			if (self.buffer[bufferLength-1]==0xa and self.buffer[bufferLength-2]==0xd and self.isParseStart==True):			
+				self.isParseStart = False
+				position = self.isParseStartIndex+2
+				extID = self.buffer[position]
+				position+=1
+				type = self.buffer[position]
+				position+=1
+				# 1 byte 2 float 3 short 4 len+string 5 double
+				if type == 1:
+					value = self.buffer[position]
+				if type == 2:
+					value = self.readFloat(position)
+					if(value<-255 or value>1023):
+						value = 0
+				if type == 3:
+					value = self.readShort(position)
+				if type == 4:
+					value = self.readString(position)
+				if type == 5:
+					value = self.readDouble(position)
+				if(type<=5):
+					self.responseValue(extID,value)
+				self.buffer = []
 
-def setSelector(i,v):
-	try:
-		selectors[i] = v
-	except IndexError,ex:
-		for _ in range(i-len(selectors)+1):
-			selectors.append(None)
-		selectors[i] = v	
+	def readFloat(self, position):
+		v = [self.buffer[position], self.buffer[position+1],self.buffer[position+2],self.buffer[position+3]]
+		return struct.unpack('<f', struct.pack('4B', *v))[0]
+	def readShort(self, position):
+		v = [self.buffer[position], self.buffer[position+1]]
+		return struct.unpack('<h', struct.pack('2B', *v))[0]
+	def readString(self, position):
+		l = self.buffer[position]
+		position+=1
+		s = ""
+		for i in Range(l):
+			s += self.buffer[position+i].charAt(0)
+		return s
+	def readDouble(self, position):
+		v = [self.buffer[position], self.buffer[position+1],self.buffer[position+2],self.buffer[position+3]]
+		return struct.unpack('<f', struct.pack('4B', *v))[0]
 
-def exitHandler(signum, frame):
-	global is_loop
-	is_loop = False
-	print "receive a signal %d, is_exit = %d"%(signum, is_loop)
+	def responseValue(self, extID, value):
+		try:	
+			print self.selectors["func%d"%extID]
+			# self.selectors[extID](value) 
+		except Exception,ex:
+			print "error",ex
 
-def float2bytes(fval):
-	val = struct.pack("f",fval)
-	return [ord(val[0]),ord(val[1]),ord(val[2]),ord(val[3])]
+	def setCallback(self,i,v):
+		self.selectors["func%d"%i] = 10
 
-def short2bytes(sval):
-	val = struct.pack("h",sval)
-	return [ord(val[0]),ord(val[1])]
+	def float2bytes(self,fval):
+		val = struct.pack("f",fval)
+		return [ord(val[0]),ord(val[1]),ord(val[2]),ord(val[3])]
 
-class ReadSerialThread(threading.Thread):
-    def run(self):
-		readLoop()
+	def short2bytes(self,sval):
+		val = struct.pack("h",sval)
+		return [ord(val[0]),ord(val[1])]
 
-print(serialPorts())
-signal.signal(signal.SIGINT, exitHandler)
-signal.signal(signal.SIGTERM, exitHandler)
-ser = serial.Serial("/dev/tty.wchusbserialfd120",115200)
+
 #ser = serial.Serial("/dev/ttyUSB0",115200)
-thread = ReadSerialThread()
-thread.setDaemon(True)
-thread.start()
+ser = mSerial()
 bot = mBot()
+print(ser.serialPorts())
 
-while True:
-	alive = thread.isAlive()
-	if not alive:
-		break
+ser.start("/dev/tty.wchusbserialfd120",bot.parseBuffer)
+
+def onLight(value):
+	print value
+
+def requestTestData():
+	global ser,bot,led
+	while True:
+		sleep(0.5)
+		try:	
+			led = 3 - led
+			ser.writePackage(bot.packageRGBLedOnBoard(0x0,0x0,0x0,0x0))
+			ser.writePackage(bot.packageRGBLedOnBoard(led,0x0,0x10,0x0))
+			# ser.writePackage(bot.packageBuzzer(led*200))
+			ser.writePackage(bot.requestLightOnBoard(8,onLight))
+		except Exception,ex:
+			print str(ex)
+
+t = Process(target = requestTestData)
+t.start()
+
+
+
 
